f168233093607a4cc6af09212ec8dec7abd7659d
Zerotonine <52973359+Zerotonine@users.noreply.github.com>
2020-12-07 13:39:13 +0100

    meep

---
+++ CSH2_Tag_09_GetPrivateMethods/CSH2_Tag_09_GetPrivateMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSH2_Tag_09_GetPrivateMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetMethod gm = null;
+            SomeMethods sm = new SomeMethods();
+
+            gm = sm.MeinePrivateMethode;
+
+            gm("hallo ich bin eine private Methode");
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ CSH2_Tag_09_GetPrivateMethods/CSH2_Tag_09_GetPrivateMethods/SomeMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSH2_Tag_09_GetPrivateMethods
+{
+    delegate void GetMethod(string s);
+    class SomeMethods
+    {
+        private void privateMethode(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public GetMethod MeinePrivateMethode
+        {
+            get
+            {
+                return new GetMethod(privateMethode);
+            }
+        }
+
+    }
+}
